9bf637cdd5fb9018e9a9f1985db2ef0375fca2bf
AA-Gabriel <gabri.vstrapasson@gmail.com>
2025-02-13 17:50:00 -0300

    Solution to Dio's challenge

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-let nome = "Tiranossauro" // aqui é a definição do nome do personagem.
-let xp = 7001 //Definindo o nível de XP será feita a verificação para verificar qual nível o personagem está.
 
-if (xp >= 0 && xp <=1000){
-    console.log("O Herói de " + nome + " está no nível de Ferro")
-} else if (xp >= 1001 && xp <=2000){
-    console.log("O Herói de " + nome + " está no nível de Bronze" )
-} else if (xp >= 2001 && xp <=5000){
-    console.log("O Herói de " + nome + " está no nível de Prata" )
-} else if (xp >= 5001 && xp <=7000){
-    console.log("O Herói de " + nome + " está no nível de Ouro" )
-} else if (xp >= 7001 && xp <=8000){
-    console.log("O Herói de " + nome + " está no nível de Plantina" )
-} else if (xp >= 8001 && xp <=9000){
-    console.log("O Herói de " + nome + " está no nível de Ascendente" )
-} else if (xp >= 9001 && xp <=10000){
-    console.log("O Herói de " + nome + " está no nível de Imortal" )
-} else{
-    console.log("O Herói de " + nome + " está no nível de Radiante" )
-}
+let heroScore = rankPlayer (91, 1)
+
+function rankPlayer (win, defeat){
+    let calcWins = win - defeat
+    const response = "O Herói tem de saldo de vitórias " + calcWins + " e está no nível de "
+    if (calcWins <= 10){
+        console.log( response + "Ferro")
+    } else if (calcWins > 10 && calcWins <= 20) {
+        console.log (response + "Bronze")
+    } else if (calcWins > 20 && calcWins <= 50) {
+        console.log (response + "Prata")
+    } else if (calcWins > 50 && calcWins <= 80) {
+        console.log (response + "Ouro")
+    } else if (calcWins > 80 && calcWins <= 90) {
+        console.log (response + "Diamante")
+    } else if (calcWins > 90 && calcWins <= 100) {
+        console.log (response + "Lendário")
+    } else 
+        console.log (response + "Imortal")
 
 
+}  
